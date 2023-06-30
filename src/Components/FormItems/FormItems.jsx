83b4f465b6e7ec419a2ced726c5310497ec6372e
Diegantos <diegantos8@gmail.com>
2023-06-30 08:15:09 +0200

    error fixed

---
+++ src/Components/FormItems/FormItems.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import "./FormItems";
+
+export const FormItems = ({handle}) => {
+
+  const [formItemsState, setFormItemsState] = useState({
+    product: "",
+    price: "",
+    quantity: "",
+  });
+
+  const { product, price, quantity } = formItemsState
+
+  const onInputChange = ({ target: { name, value}}) => {
+    setFormItemsState({
+        ...formItemsState,
+        [name]: value
+    })
+  }
+
+  const onInvoiceItemSubmit = (event) => {
+    event.preventDefault();
+
+    if (product.trim().length <= 1) return;
+    if (price.trim().length <= 1) return;
+    if (isNaN(price.trim()))
+      return alert("El precio debe ser un valor numérico");
+    if (quantity.trim().length < 1) return;
+    if (isNaN(quantity.trim()))
+      return alert("La cantidad debe ser un valor numérico mayor de 0");
+
+    handle(formItemsState);
+
+    setFormItemsState({
+        product:'',
+        price:'',
+        value:'',
+    })
+  };
+
+  return (
+    <>
+      <form className="w-50" onSubmit={onInvoiceItemSubmit}>
+        <input
+          type="text"
+          name="product"
+          placeholder="Producto"
+          className="form-control mt-3 mb-3"
+          value={product}
+          onChange={onInputChange}
+        />
+        <input
+          type="text"
+          name="price"
+          placeholder="Precio"
+          className="form-control mb-3"
+          value={price}
+          onChange={event => onInputChange(event)}
+        />
+        <input
+          type="text"
+          name="quantity"
+          placeholder="Cantidad"
+          className="form-control mb-3"
+          value={quantity}
+          onChange={onInputChange}
+        />
+        <button type="submit" className="btn btn-primary">
+          Añadir producto
+        </button>
+      </form>
+    </>
+  );
+};
--- src/Components/InvoiceApp/InvoiceApp.jsx
+++ src/Components/InvoiceApp/InvoiceApp.jsx
@@ -6,57 +6,37 @@ import { ListItemsView } from '../ListItemsView/ListItemsView'
 import { getInvoice, calculateTotal } from '../Services/getInvoice'
 import { Totalview } from '../TotalView/TotalView'
 import './InvoiceApp'
+import { FormItems } from '../FormItems/FormItems'
 
 const invoiceInitial = {
-  id: 10,
-  name: "Componentes Pc",
+  id: 0,
+  name: "",
   client: {
-    name: "Pepe",
-    lastName: "Doe",
+    name: "",
+    lastName: "",
     address: {
-      country: "Spain",
-      city: "Madrid",
-      street: "Calle Uno",
+      country: "",
+      city: "",
+      street: "",
       number: 12,
     },
   },
   company: {
-    name: "Mi propia compañia",
-    fiscalNumber: 123456,
+    name: "",
+    fiscalNumber: 0,
   },
-  items: [
-    {
-      id: 1,
-      product: "Cpu Intel I7",
-      price: 499,
-      quantity: 1,
-    },
-    {
-      id: 2,
-      product: "Corsair Keyboard Mecanico",
-      price: 150,
-      quantity: 2,
-    },
-    {
-      id: 3,
-      product: "Monitor Asus",
-      price: 350,
-      quantity: 1,
-    },
-  ],
+  items: []
 };
 
 export const InvoiceApp = () => {
 
-    const [ invoice, setInvoice ] = useState(invoiceInitial)
+    const [invoice, setInvoice ] = useState(invoiceInitial)
+    const { id, name, client, company } = invoice;
 
     const [items, setItems] = useState([]);
 
-    const [productValue, setProductValue] = useState("");
-    const [priceValue, setPriceValue] = useState("");
-    const [quantityValue, setQuantityValue] = useState("");
+    const [total, setTotal] = useState(0);
 
-    const [total, setTotal] = useState(0)
     const [counter, setCounter] = useState(4);
 
     useEffect(() => {
@@ -66,54 +46,22 @@ export const InvoiceApp = () => {
     }, [])
 
     useEffect(() => {
-      console.log('Price, product o quantity has changed')
-    },[priceValue, productValue, quantityValue])
+      setTotal(calculateTotal(items));
+    }, [items]);
 
-    useEffect(() => {
-      console.log('Counter has changed')
-    },[counter])
+    const handleAddItems = ({product, price, quantity}) => {
 
-    useEffect(() => {
-      console.log('Items has changed')
-      setTotal(calculateTotal(items))
-    },[items])
-
-    // const invoice = getInvoice()
-    const { id, name, client, company } = invoice;
-
-    // const [ invoiceItemsState, setInvoiceItemsState ] = useState({
-    //   productValue: '',
-    //   priceValue: '',
-    //   quantityValue: '',
-    // })
-
-    // const { productValue, priceValue, quantityValue } = invoiceItemsState
-
-    const onInvoiceItemSubmit = (event) => {
-      event.preventDefault();
-
-      if (productValue.trim().length <= 1) return;
-      if (priceValue.trim().length <= 1) return;
-      if (isNaN(priceValue.trim()))
-        return alert("El precio debe ser un valor numérico");
-      if (quantityValue.trim().length < 1) return;
-      if (isNaN(quantityValue.trim()))
-        return alert("La cantidad debe ser un valor numérico mayor de 0");
-
-      setItems([
-        ...items,
+      setItems([...items,
         {
-          id: { counter },
-          product: productValue.trim(),
-          price: +priceValue.trim(), //dos formas de convertir a Int
-          quantity: parseInt(quantityValue.trim(), 10),
+          id: counter,
+          product: product.trim(),
+          price: +price.trim(), //dos formas de convertir a Int
+          quantity: parseInt(quantity.trim(), 10),
         },
       ]);
-      setProductValue("");
-      setPriceValue("");
-      setQuantityValue("");
+
       setCounter(counter + 1);
-    }
+    };
 
     return (
       <>
@@ -137,42 +85,7 @@ export const InvoiceApp = () => {
 
               <ListItemsView title="Productos de la factura" items={items} />
               <Totalview total={total} />
-
-              <form className="w-50" onSubmit={onInvoiceItemSubmit}>
-                <input
-                  type="text"
-                  name="product"
-                  placeholder="Producto"
-                  className="form-control mt-3 mb-3"
-                  value={productValue}
-                  onChange={(event) => {
-                    setProductValue(event.target.value);
-                  }}
-                />
-                <input
-                  type="text"
-                  name="price"
-                  placeholder="Precio"
-                  className="form-control mb-3"
-                  value={priceValue}
-                  onChange={(event) => {
-                    setPriceValue(event.target.value);
-                  }}
-                />
-                <input
-                  type="text"
-                  name="quantity"
-                  placeholder="Cantidad"
-                  className="form-control mb-3"
-                  value={quantityValue}
-                  onChange={(event) => {
-                    setQuantityValue(event.target.value);
-                  }}
-                />
-                <button type="submit" className="btn btn-primary">
-                  Añadir producto
-                </button>
-              </form>
+              <FormItems handle={ (newItem) => handleAddItems(newItem) } />
             </div>
           </div>
         </div>
